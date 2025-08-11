39cff3f2357cc7abb11e933c507674da967ce68d
Hassan-Eid-Hassan <hassaneid339@gmail.com>
2025-08-11 20:51:15 +0300

    add sharedlib

--- src/com/iti/docker.groovy
+++ src/com/iti/docker.groovy
@@ -3,3 +3,11 @@ package com.iti;
 def build(IMAGE_NAME, IMAGE_TAG){
     sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
 }
+
+def login(USERNAME, PASSWORD){
+    sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+}
+
+def push(IMAGE_NAME, IMAGE_TAG){
+    sh "docker push ${IMAGE_NAME}:${IMAGE_TAG}"
+}
